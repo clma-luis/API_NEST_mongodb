5b6a0e05438d1d04152e9791da57cec2caf6c0ec
carloslmarin <marincarlosl19@gmail.com>
2022-02-15 22:55:35 -0500

    api-backend-nestjs

---
+++ src/app.service.ts
@@ -0,0 +1,16 @@
+// src/app.service.ts
+import { Injectable, Inject } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import config from './config'; //
+
+@Injectable()
+export class AppService {
+  constructor(
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+  ) {}
+  getHello(): string {
+    const apiKey = this.configService.apiKey;
+    const name = this.configService.database.name;
+    return `Hello World! ${apiKey} ${name}`;
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('Ecommerce project')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  app.enableCors(); // allow to everyone make a request outside of the domain's API
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
