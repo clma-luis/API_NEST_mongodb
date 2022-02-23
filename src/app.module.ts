51d417540e29ef7c0fcabe564bfe91efdc217629
carloslmarin <marincarlosl19@gmail.com>
2022-02-22 22:15:08 -0500

    new commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,6 +23,7 @@ import config from './config';
         JWT_SECRET: Joi.string().required(),
         DATABASE_NAME: Joi.string().required(),
         DATABASE_PORT: Joi.number().required(),
+        MONGO_CONNECTION: Joi.string().required(),
       }),
     }),
     UsersModule,
